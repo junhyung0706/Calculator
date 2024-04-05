bfa1e7daef1eee90cae3691321b0a4c97fd1f99f
믿음직한 메추라기 <72088449+junhyung0706@users.noreply.github.com>
2024-04-05 12:15:41 +0900

    Update Multiplier.v
    주석을 달았다.

--- Multiplier.v
+++ Multiplier.v
@@ -1,8 +1,8 @@
 module Multiplier(
-    input [7:0] A, B,
-    output [15:0] RESULT
+    input [7:0] A, B,		//8비트 크기의 A, B 입력
+    output [15:0] RESULT	//16비트 크기의 RESULT 출력
 );
 
-assign RESULT = A * B;
+assign RESULT = A * B;	//A와 B가 8비트 이므로 최대 값은 255 * 255 = 65025이고 이는 16비트에 저장 할 수 있다.
 
 endmodule
