197f48386fe30c1180f130259fdcba063e3aeb86
믿음직한 메추라기 <72088449+junhyung0706@users.noreply.github.com>
2024-04-05 12:13:44 +0900

    Update Adder.v
    주석을 달았다.

--- Adder.v
+++ Adder.v
@@ -1,8 +1,8 @@
 module Adder(
-    input [7:0] A, B,
-    output [15:0] RESULT
+    input [7:0] A, B,		// 8비트 크기의 A와 B
+    output [15:0] RESULT	// 16비트 크기의 RESULT
 );
 
-assign RESULT = A + B;
+assign RESULT = A + B;	    // A와 B가 8비트 이므로 최대 합은 255+255=510이고 16비트 변수에 저장 가능
 
 endmodule
